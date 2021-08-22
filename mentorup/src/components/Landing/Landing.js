d0fcf8fd706d7a1e73924ee1e62e320c57d65c9a
m-fidow <muna.a.fidow@gmail.com>
2021-08-22 16:11:40 +0100

    added H1 to landing
    Co-authored-by: Clarisse catclaclou@hotmail.fr
    Co-authored-by: Toby Toby.D.Smith@Outlook.com

--- mentorup/src/components/Landing/Landing.js
+++ mentorup/src/components/Landing/Landing.js
@@ -1,9 +1,13 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
 import Header from "../Header/Header";
 function Landing() {
   return (
     <div>
       <Header />
+      <Typography variant="h5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+      </Typography>
     </div>
   );
 }
