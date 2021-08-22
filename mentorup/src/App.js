6bc99643913b6de1a68d0ddba1d567544081c9ec
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-22 13:59:18 +0100

    Added libraries

---
+++ mentorup/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button color="primary">Hello World</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ mentorup/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "@fontsource/roboto";
+import { FirebaseAppProvider } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAskJ-GQp_WSP1XugdxLFYEdXi1g58xL3s",
+  authDomain: "mentorup-f74a4.firebaseapp.com",
+  projectId: "mentorup-f74a4",
+  storageBucket: "mentorup-f74a4.appspot.com",
+  messagingSenderId: "470552212684",
+  appId: "1:470552212684:web:87b3d1782e3ac0ed463e69",
+  measurementId: "G-VR55NE6RZ7",
+};
+
+ReactDOM.render(
+  <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </FirebaseAppProvider>,
+  document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
