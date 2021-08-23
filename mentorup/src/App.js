509d312b16e0e47138212c9d1bec7479d2cf34e0
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-23 19:27:19 +0100

    Started adding textfield for contact form

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import { Button } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import Homepage from "./components/Homepage/Homepage";
 import Landing from "./components/Landing/Landing";
+import ContactForm from "./components/contact-form/contact";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Landing />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage}></Route>
+          <Route path="/landing" component={Landing}></Route>
+          <Route path="/contact" component={ContactForm}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- mentorup/src/components/contact-form/contact.js
+++ mentorup/src/components/contact-form/contact.js
@@ -1,12 +1,46 @@
 import { Button, Typography } from "@material-ui/core";
 import React from "react";
-import Header from "../Header/Header";
 import Landing from "../Landing/Landing";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+
+//default material ui theme example TO CHANGE
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "75ch",
+    },
+  },
+}));
 
 function ContactForm() {
+  const classes = useStyles();
   return (
     <div>
-      <Header />
+      <p>Hello there, this is a contact form</p>
+      <form className={classes.root} noValidate autoComplete="off">
+        <div>
+          <TextField
+            required
+            id="outlined-required"
+            label="Email (required)"
+            defaultValue="Type your email here"
+            variant="outlined"
+          />
+        </div>
+        <div>
+          <TextField
+            required
+            id="outlined-multiline-static"
+            label="Message (required)"
+            multiline
+            rows={10}
+            defaultValue="Please enter your message here"
+            variant="outlined"
+          />
+        </div>
+      </form>
     </div>
   );
 }
