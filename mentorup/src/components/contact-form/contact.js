b59efa210e5a81b0c5bfebad16f653bb432b8702
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-23 18:37:11 +0100

    Created Contact Form branch

---
+++ mentorup/src/components/contact-form/contact.js
@@ -0,0 +1,14 @@
+import { Button, Typography } from "@material-ui/core";
+import React from "react";
+import Header from "../Header/Header";
+import Landing from "../Landing/Landing";
+
+function ContactForm() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default ContactForm;
