1ebccbc069672e68eb38ef990c6ff18f661ce3a1
m-fidow <muna.a.fidow@gmail.com>
2021-08-25 19:26:47 +0100

    added h1

--- mentorup/src/assets/components/Helpselection/Helpselection.js
+++ mentorup/src/assets/components/Helpselection/Helpselection.js
@@ -1,16 +1,26 @@
 import React from "react";
-import { InputLabel, MenuItem, TextField } from "@material-ui/core";
+import {
+  Box,
+  MenuItem,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
 function Helpselection() {
   return (
-    <div>
-      <InputLabel id="subject">I need help with</InputLabel>
+    <Box component="div" width={300} display="flex" flexDirection="column">
+      <Typography variant="h6"> What do you need help with?</Typography>
+
       <TextField id="subject" label="Select a subject" select>
         <MenuItem value="Pitching">Preparing a pitch</MenuItem>
         <MenuItem value="Business Plan">Business Plan</MenuItem>
         <MenuItem value="Business Plan">Business Plan</MenuItem>
         <MenuItem value="Business Plan">Business Plan</MenuItem>
       </TextField>
-    </div>
+      <Button variant="contained" color="primary">
+        Next
+      </Button>
+    </Box>
   );
 }
 
