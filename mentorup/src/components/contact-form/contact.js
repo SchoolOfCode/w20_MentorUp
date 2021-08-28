2a08cb002f855a66feb41a1ab590a64e2f5f0132
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-08-28 11:32:23 +0100

    Added mentor's infor fetched from database

--- mentorup/src/components/contact-form/contact.js
+++ mentorup/src/components/contact-form/contact.js
@@ -3,6 +3,9 @@ import React, { useEffect } from "react";
 import Landing from "../Landing/Landing";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
+import { Avatar } from "@material-ui/core";
+
 import { useParams } from "react-router-dom";
 import "firebase/firestore";
 import { useFirestoreDocData, useFirestore } from "reactfire";
@@ -13,6 +16,10 @@ const useStyles = makeStyles((theme) => ({
     "& .MuiTextField-root": {
       margin: theme.spacing(1),
       width: "75ch",
+    },
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
     },
   },
 }));
@@ -65,6 +72,23 @@ function ContactForm() {
        <Button variant="contained" color="primary" onClick={}>
         Send
       </Button> */}
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-around"
+        alignItems="center"
+      >
+        <Avatar
+          alt="Avatar of the mentor"
+          src={data.avatar}
+          className={classes.large}
+        />
+        <p>Username: {data.username}</p>
+        <p>Industry: {data.industry}</p>
+        <p>Business Stage: {data.businessStage}</p>
+        <p>Expertise: {data.expertise}</p>
+      </Grid>
+
       <h3>
         {" "}
         To contact your mentor, press the button below to be redirected to your
