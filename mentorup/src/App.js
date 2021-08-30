2e58a6761a3191de001375d336cf778a3de4b6ff
m-fidow <muna.a.fidow@gmail.com>
2021-08-30 16:45:08 +0100

    bsl selection with options

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Button } from "@material-ui/core";
 
 import "./App.css";
+import BSLSelection from "./components/BSLSelection/BSLSelection";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button color="primary">Hello World</Button>
-      </header>
+      <BSLSelection />
     </div>
   );
 }
