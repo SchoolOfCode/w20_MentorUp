abd1b1bbd8b696bd28aac8f737793a819fddf0da
m-fidow <muna.a.fidow@gmail.com>
2021-08-30 13:42:36 +0100

    added radio group

--- mentorup/src/components/MentorOrMentee/MentorOrMentee.js
+++ mentorup/src/components/MentorOrMentee/MentorOrMentee.js
@@ -1,7 +1,33 @@
+import {
+  Typography,
+  FormControl,
+  FormLabel,
+  FormControlLabel,
+  RadioGroup,
+  Radio,
+} from "@material-ui/core";
 import React from "react";
 
 function MentorOrMentee() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <Typography variant="h5">Create your profile</Typography>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">Gender</FormLabel>
+        <RadioGroup aria-label="gender" name="gender1">
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+          <FormControlLabel
+            value="disabled"
+            disabled
+            control={<Radio />}
+            label="(Disabled option)"
+          />
+        </RadioGroup>
+      </FormControl>
+    </div>
+  );
 }
 
 export default MentorOrMentee;
