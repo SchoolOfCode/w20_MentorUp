89dbb98ab314a892772e3e1f08dd0dfdff5ed86c
m-fidow <muna.a.fidow@gmail.com>
2021-08-30 16:48:27 +0100

    initialised component folder

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 import { Button } from "@material-ui/core";
 
 import "./App.css";
+import ElevatorPitch from "./components/ElevatorPitch/ElevatorPitch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Button color="primary">Hello World</Button>
-      </header>
+      <ElevatorPitch />
     </div>
   );
 }
---
+++ mentorup/src/components/ElevatorPitch/ElevatorPitch.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ElevatorPitch() {
+  return <div>Hello from elevator pitch</div>;
+}
+
+export default ElevatorPitch;
