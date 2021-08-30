17dba8b3f2d46999f714dcea7b7a8bea9d5cc682
m-fidow <muna.a.fidow@gmail.com>
2021-08-30 14:17:03 +0100

    added grid styling

--- mentorup/src/components/MentorOrMentee/MentorOrMentee.js
+++ mentorup/src/components/MentorOrMentee/MentorOrMentee.js
@@ -1,48 +1,63 @@
 import {
   Typography,
   FormControl,
-  FormLabel,
+  withStyles,
   FormControlLabel,
   RadioGroup,
   Radio,
   Button,
+  Grid,
 } from "@material-ui/core";
+
 import React from "react";
-import { makeStyles } from "@material-ui/core";
-import { classes } from "istanbul-lib-coverage";
-const useStyles = makeStyles((theme) => ({
-  radio: {
-    "&$checked": {
-      backgroundColor: "#4B8DF8",
-    },
-  },
-  checked: {},
-}));
+
 function MentorOrMentee() {
-  const classes = useStyles();
+  // const classes = useStyles();
   return (
-    <div>
-      <Typography variant="h5">Create your profile</Typography>
-      <FormControl component="fieldset">
-        <RadioGroup aria-label="gender" name="gender1">
-          <FormControlLabel
-            value="mentee"
-            onChange
-            classes={{ root: classes.radio, checked: classes.checked }}
-            control={<Radio />}
-            label="I need a mentor"
-          />
-          <FormControlLabel
-            value="mentor"
-            control={<Radio />}
-            label="I want to be a mentor"
-          />
-        </RadioGroup>
-        <Button variant="contained" color="primary" fullWidth>
-          Next
-        </Button>
-      </FormControl>
-    </div>
+    <Grid
+      style={{
+        margin: "0 auto",
+      }}
+      sm={6}
+      md={3}
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid
+        item
+        container
+        direction="column"
+        spacing={4}
+        // justifyContent="center"
+        // alignItems="space-between"
+      >
+        <Grid item>
+          <Typography variant="h5" align="center">
+            Create your profile
+          </Typography>
+        </Grid>
+
+        <FormControl component="fieldset">
+          <RadioGroup aria-label="mentorormentee" name="mentorormentee">
+            <FormControlLabel
+              value="mentee"
+              control={<Radio />}
+              label="I need a mentor"
+            />
+            <FormControlLabel
+              value="mentor"
+              control={<Radio />}
+              label="I want to be a mentor"
+            />
+          </RadioGroup>
+          <Button variant="contained" color="primary" fullWidth>
+            Next
+          </Button>
+        </FormControl>
+      </Grid>
+    </Grid>
   );
 }
 
