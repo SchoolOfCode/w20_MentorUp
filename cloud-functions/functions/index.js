4c03861fcbd24f3b8f4e9cb13668af0a8bb2b48c
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-09-02 18:21:51 +0100

    Email sending properly thourgh sendGrid + rules

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+sgMail.setApiKey(functions.config().sendgrid.key);
 
 // The Firebase Admin SDK to access Firestore.
 const admin = require("firebase-admin");
@@ -8,13 +8,9 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-// EXAMPLE
-// Listens for new messages added to /messages/:documentId/original
-// and creates an uppercase version of the message
-// to / messages /: documentId / uppercase
-exports.makeUppercase = functions.firestore
+exports.sendEmail = functions.firestore
   .document("/helpRequests/{documentId}")
-  .onUpdate((snap, context) => {
+  .onCreate((snap, context) => {
     // get original message (text content) and
     // Find the email address associated with the mentor ID
     // (map through your collection until perfect match is found)
@@ -22,22 +18,43 @@ exports.makeUppercase = functions.firestore
     // and send message to email address
 
     // Grab the current value of what was written to Firestore.
-    const helpRequest = snap.data().original;
+    const helpRequest = snap.data();
 
-    let mentor = db.doc("userData/" + helpRequest.authenticationID).get();
+    db.collection("userData")
+      .where("authenticationID", "==", helpRequest.mentorID)
+      .where("type", "==", "mentor")
+      .get()
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          // no result, so return since we cannot do anything
+          console.log(
+            "Could not find a matching record for " + helpRequest.mentorID
+          );
+          return;
+        } else {
+          // grab out the first record from our query above
+          const mentorData = snapshot.docs[0].data();
 
-    const msg = {
-      to: mentor.email,
-      from: "test@example.com", // Use the email address or domain you verified above
-      subject: "Help Request from a new mentee",
-      text: helpRequest.message,
-      html: helpRequest.message,
-    };
+          const msg = {
+            to: `${mentorData.email}`,
+            from: "catclaclou@hotmail.fr",
+            subject: "Help Request from a new mentee",
+            text: helpRequest.message,
+            html: helpRequest.message,
+          };
 
-    return sgMail.send(msg).then(() => {});
+          console.log("The email is:" + mentorData.email);
+          console.log("The text is:" + helpRequest.message);
+          console.log("The html is:" + helpRequest.message);
 
-    // You must return a Promise when performing asynchronous tasks
-    // inside a Functions such as writing to Firestore.
-    // Setting an 'uppercase' field in Firestore document returns a Promise.
-    // return snap.ref.set({ uppercase }, { merge: true });
+          return sgMail
+            .send(msg)
+            .then(() => {
+              console.log("Sent email successfully!");
+            })
+            .catch((e) => {
+              console.log("Could not send the email because of: " + e);
+            });
+        }
+      });
   });
