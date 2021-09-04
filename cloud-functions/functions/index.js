116a9f9b18b9f97fad107302f0621a3657592645
Clarisse <78910159+Claryyy@users.noreply.github.com>
2021-09-04 11:53:27 +0100

    Contact form hooked to firebase!

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -7,54 +7,64 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 const db = admin.firestore();
+const userDb = admin.auth();
 
 exports.sendEmail = functions.firestore
   .document("/helpRequests/{documentId}")
   .onCreate((snap, context) => {
     // get original message (text content) and
-    // Find the email address associated with the mentor ID
-    // (map through your collection until perfect match is found)
-    // Once a match is found connect to sendgrid
-    // and send message to email address
 
     // Grab the current value of what was written to Firestore.
     const helpRequest = snap.data();
 
-    db.collection("userData")
-      .where("authenticationID", "==", helpRequest.mentorID)
-      .where("type", "==", "mentor")
+    // Find the email address associated with the mentor ID
+    db.doc("userData/" + helpRequest.mentorID)
       .get()
       .then((snapshot) => {
-        if (snapshot.empty) {
+        if (snapshot.exists) {
+          // grab out the first record from our query above
+          const mentorData = snapshot.data();
+
+          userDb.getUser(mentorData.authenticationID).then((userRecord) => {
+            if (userRecord === null) {
+              console.log(
+                "Could not find a matching user record for " +
+                  mentorData.authenticationID
+              );
+              return;
+            }
+
+            // now the auth user is found use the email on that record to send
+
+            // connect to sendgrid
+            // and send message to email address
+            const msg = {
+              to: `${userRecord.email}`,
+              from: "catclaclou@hotmail.fr",
+              subject: "Help Request from a new mentee",
+              text: helpRequest.message,
+              html: helpRequest.message,
+            };
+
+            console.log("The email is:" + userRecord.email);
+            console.log("The text is:" + helpRequest.message);
+            console.log("The html is:" + helpRequest.message);
+
+            return sgMail
+              .send(msg)
+              .then(() => {
+                console.log("Sent email successfully!");
+              })
+              .catch((e) => {
+                console.log("Could not send the email because of: " + e);
+              });
+          });
+        } else {
           // no result, so return since we cannot do anything
           console.log(
             "Could not find a matching record for " + helpRequest.mentorID
           );
           return;
-        } else {
-          // grab out the first record from our query above
-          const mentorData = snapshot.docs[0].data();
-
-          const msg = {
-            to: `${mentorData.email}`,
-            from: "catclaclou@hotmail.fr",
-            subject: "Help Request from a new mentee",
-            text: helpRequest.message,
-            html: helpRequest.message,
-          };
-
-          console.log("The email is:" + mentorData.email);
-          console.log("The text is:" + helpRequest.message);
-          console.log("The html is:" + helpRequest.message);
-
-          return sgMail
-            .send(msg)
-            .then(() => {
-              console.log("Sent email successfully!");
-            })
-            .catch((e) => {
-              console.log("Could not send the email because of: " + e);
-            });
         }
       });
   });
