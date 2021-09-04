130bef628b46e36112893ad29452d2dd63c928ef
m-fidow <muna.a.fidow@gmail.com>
2021-09-04 11:55:36 +0100

    changed height of AppBar

--- mentorup/src/components/Header/Header.js
+++ mentorup/src/components/Header/Header.js
@@ -4,11 +4,18 @@
 import { AppBar, Toolbar, Typography } from "@material-ui/core";
 import Logo from "../../assets/logo.svg";
 import HamburgerMenu from "./HamburgerMenu";
-
+import { makeStyles } from "@material-ui/core";
+const useStyles = makeStyles((theme) => ({
+  header: {
+    height: "80px",
+    justifyContent: "center",
+  },
+}));
 function Header() {
+  const classes = useStyles();
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.header}>
         <Toolbar>
           <HamburgerMenu />
           <Typography variant="h6">MentorUP</Typography>
