649c8e661c7eaa4f55e5cef0183568dcafb9a1f3
m-fidow <muna.a.fidow@gmail.com>
2021-09-06 19:27:53 +0100

    added image

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Button, Grid } from "@material-ui/core";
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import HomePage from "./components/HomePage/HomePage";
--- mentorup/src/components/HomePage/HomePage.js
+++ mentorup/src/components/HomePage/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Grid, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
+import heroImage from "../../assets/heroimage.jpg";
 const useStyles = makeStyles((theme) => ({
   mainLayout: {
     flexDirection: "column",
     alignContent: "'flex-start',",
-    backgroundColor: "green",
+    // backgroundColor: "green",
     padding: "16px",
   },
   h1Override: {
@@ -16,7 +17,7 @@ function HomePage() {
   const classes = useStyles();
   return (
     <section style={{ padding: "16px" }}>
-      <Grid container sm={9} className={classes.mainLayout} spacing={2}>
+      <Grid container xs={12} sm={9} className={classes.mainLayout} spacing={2}>
         <Grid item>
           <Typography variant="h1" className={classes.h1Override} gutterBottom>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -34,6 +35,9 @@ function HomePage() {
           <Button variant="outlined" color="primary" fullWidth>
             Log in
           </Button>
+        </Grid>
+        <Grid item>
+          <img src={heroImage} alt="hero image" style={{ maxWidth: "100%" }} />
         </Grid>
       </Grid>
     </section>
