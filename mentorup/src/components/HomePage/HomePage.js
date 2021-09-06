2ebefeb8d708bb88a3607bfcf47190a9f5b69c09
m-fidow <muna.a.fidow@gmail.com>
2021-09-06 19:23:31 +0100

    added section and log in button

--- mentorup/src/components/HomePage/HomePage.js
+++ mentorup/src/components/HomePage/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 const useStyles = makeStyles((theme) => ({
   mainLayout: {
@@ -15,14 +15,28 @@ const useStyles = makeStyles((theme) => ({
 function HomePage() {
   const classes = useStyles();
   return (
-    <Grid container sm={9} className={classes.mainLayout}>
-      <Typography variant="h1" className={classes.h1Override} gutterBottom>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-      </Typography>
-      <Typography variant="subtitle1">
-        Tristique nunc phasellus consectetur.
-      </Typography>
-    </Grid>
+    <section style={{ padding: "16px" }}>
+      <Grid container sm={9} className={classes.mainLayout} spacing={2}>
+        <Grid item>
+          <Typography variant="h1" className={classes.h1Override} gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </Typography>
+          <Typography variant="subtitle1">
+            Tristique nunc phasellus consectetur.
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" color="primary" fullWidth>
+            Sign up
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" color="primary" fullWidth>
+            Log in
+          </Button>
+        </Grid>
+      </Grid>
+    </section>
   );
 }
 
