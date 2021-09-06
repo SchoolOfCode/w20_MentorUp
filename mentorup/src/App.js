8e419bce44ccfd41c026c24ed8f49437aa9a095d
m-fidow <muna.a.fidow@gmail.com>
2021-09-06 18:37:58 +0100

    hooked up homepage component

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -4,12 +4,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Button, Grid } from "@material-ui/core";
 import "./App.css";
 import Header from "./components/Header/Header";
+import HomePage from "./components/HomePage/HomePage";
 // import Header from "./components/Header";
 function App() {
   return (
-    <Router>
-      <Header />
-    </Router>
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
