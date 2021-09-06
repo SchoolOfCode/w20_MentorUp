046d3639f2b60d0bfa23043ff2f0f1d9d113b6f2
m-fidow <muna.a.fidow@gmail.com>
2021-09-06 18:51:27 +0100

    styled h1

--- mentorup/src/components/HomePage/HomePage.js
+++ mentorup/src/components/HomePage/HomePage.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import { Grid, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+const useStyles = makeStyles((theme) => ({
+  mainLayout: {
+    flexDirection: "column",
+    alignContent: "'flex-start',",
+    backgroundColor: "green",
+    padding: "16px",
+  },
+  h1Override: {
+    fontSize: "1.875rem",
+  },
+}));
 function HomePage() {
-  return <div>Hello</div>;
+  const classes = useStyles();
+  return (
+    <Grid container sm={9} className={classes.mainLayout}>
+      <Typography variant="h1" className={classes.h1Override}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+      </Typography>
+    </Grid>
+  );
 }
 
 export default HomePage;
