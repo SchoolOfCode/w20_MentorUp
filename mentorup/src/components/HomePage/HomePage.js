a2c8c1ede62e1f09ea3878e63edfab5eff3dac11
m-fidow <muna.a.fidow@gmail.com>
2021-09-09 00:03:37 +0100

    refactored how it works section to make the code dry

--- mentorup/src/components/HomePage/HomePage.js
+++ mentorup/src/components/HomePage/HomePage.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {
-  Grid,
-  Typography,
-  Button,
-  Icon,
-  Divider,
-  Box,
-  Container,
-} from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+// import { Button, Icon, Divider, Box, Container } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core";
 import heroImage from "../../assets/heroimage.jpg";
 import logo from "../../assets/logo.svg";
 import dummy1 from "../../assets/undraw_fall_thyk.svg";
+import dummy2 from "../../assets/undraw_my_app_re_gxtj.svg";
+import dummy3 from "../../assets/undraw_step_to_the_sun_nxqq.svg";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
-import { spacing } from "@material-ui/system";
+
 const useStyles = makeStyles((theme) => ({
   mainLayout: {
     // flexDirection: "column",
@@ -48,8 +48,14 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
+const howItWorksSteps = [
+  { step: 1, text: "Sign up", image: dummy1 },
+  { step: 2, text: "Fill out form", image: dummy2 },
+  { step: 3, text: "Match with mentor!", image: dummy3 },
+];
 function HomePage() {
   const classes = useStyles();
+  console.log(howItWorksSteps[0].image2);
   return (
     <main style={{ padding: "16px" }}>
       <section>
@@ -123,51 +129,28 @@ function HomePage() {
             <Typography variant="h4">How it works</Typography>
           </Grid>
           <Grid container>
-            <Grid
-              item
-              container
-              sm={12}
-              md={4}
-              justifyContent="center"
-              style={{ padding: "24px" }}
-            >
-              <Typography variant="h6">Step 1: Blabla</Typography>
-              <img
-                src={dummy1}
-                style={{ maxWidth: "100%", alignSelf: "center" }}
-                alt="dummy"
-              />
-            </Grid>
-            <Grid
-              item
-              container
-              sm={12}
-              md={4}
-              justifyContent="center"
-              style={{ padding: "24px" }}
-            >
-              <Typography variant="h6">Step 1: Blabla</Typography>
-              <img
-                src={dummy1}
-                style={{ maxWidth: "100%", alignSelf: "center" }}
-                alt="dummy"
-              />
-            </Grid>
-            <Grid
-              item
-              container
-              sm={12}
-              md={4}
-              justifyContent="center"
-              style={{ padding: "24px" }}
-            >
-              <Typography variant="h6">Step 1: Blabla</Typography>
-              <img
-                src={dummy1}
-                style={{ maxWidth: "100%", alignSelf: "center" }}
-                alt="dummy"
-              />
-            </Grid>
+            {howItWorksSteps.map((item, index) => {
+              return (
+                <Grid
+                  item
+                  container
+                  sm={12}
+                  md={4}
+                  justifyContent="center"
+                  style={{ padding: "24px" }}
+                >
+                  <Typography variant="h5" gutterBottom>
+                    Step {item.step}: {item.text}
+                  </Typography>
+                  <img
+                    src={item.image}
+                    style={{ maxWidth: "100%", alignSelf: "center" }}
+                    alt={item.text}
+                  />
+                  ;
+                </Grid>
+              );
+            })}
           </Grid>
         </Grid>
       </section>
