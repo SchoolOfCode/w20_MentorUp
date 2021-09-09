38f9244b937beff05ef565c317ca0ba5f09e484b
m-fidow <muna.a.fidow@gmail.com>
2021-09-09 20:53:15 +0100

    put the footer as a new component and routed it

--- mentorup/src/App.js
+++ mentorup/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import HomePage from "./components/HomePage/HomePage";
 // import Header from "./components/Header";
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
         </Switch>
+        <Footer />
       </Router>
     </>
   );
