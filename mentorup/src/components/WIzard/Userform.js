5358f59aa66742ee861c061cb7eaec85d55f81d3
m-fidow <muna.a.fidow@gmail.com>
2021-09-10 22:46:23 +0100

    created steps and initialised userform component

---
+++ mentorup/src/components/WIzard/Userform.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+function Userform() {
+  //Steps
+  const [activeSteps, setActiveSteps] = useState(0);
+
+  return <div></div>;
+}
+
+export default Userform;
