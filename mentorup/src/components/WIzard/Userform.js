033b1fdb31da91d3d72d22d09e3e91a7b8d1619e
m-fidow <muna.a.fidow@gmail.com>
2021-09-10 22:50:09 +0100

    created steps function

--- mentorup/src/components/WIzard/Userform.js
+++ mentorup/src/components/WIzard/Userform.js
@@ -3,6 +3,17 @@ import React, { useState } from "react";
 function Userform() {
   //Steps
   const [activeSteps, setActiveSteps] = useState(0);
+  const getSteps = () => {
+    return [
+      "UsernameAvatar",
+      "MentorMentee",
+      "BusinessStage",
+      "Industry",
+      "BSL",
+      "Language",
+    ];
+  };
+  const steps = getSteps();
 
   return <div></div>;
 }
