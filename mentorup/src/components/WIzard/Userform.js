efa515271617241e817682c7d2b3ebeaacab1a20
m-fidow <muna.a.fidow@gmail.com>
2021-09-10 22:55:30 +0100

    created states for values

--- mentorup/src/components/WIzard/Userform.js
+++ mentorup/src/components/WIzard/Userform.js
@@ -14,7 +14,27 @@ function Userform() {
     ];
   };
   const steps = getSteps();
+  //State variables
+  const [multiFormValues, setMultiFormValues] = useState({
+    username: "",
+    avatar: "",
+    mentorMentee: "",
+    subject: "",
+    industry: "",
+    businessStage: "",
+    yearsInBusiness: "",
+    language: "",
+  });
+  //Navigates to the next page
 
+  const handleNext = () => {
+    setActiveSteps((nextStep) => nextStep + 1);
+  };
+  //Navigates to the next pageBreakAfter:
+
+  const handleNext = () => {
+    setActiveSteps((nextStep) => nextStep + 1);
+  };
   return <div></div>;
 }
 
